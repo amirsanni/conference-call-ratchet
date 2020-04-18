becff291b650b8c89592f7195e62eaabc24fb0a7
Amir Sanni <amirsanni@gmail.com>
2020-04-18 10:58:15 +0100

    Update gitignore

---
+++ ws/bin/server.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Description of server
+ *
+ * @author Amir <amirsanni@gmail.com>
+ * @date 23-Dec-2016
+ */
+
+require 'vendor/autoload.php';
+
+use Amir\Comm;
+use Ratchet\App;
+
+//set an array of origins allowed to connect to this server
+$allowed_origins = ['localhost', '127.0.0.1'];
+
+// Run the server application through the WebSocket protocol on port 8080
+$app = new App('localhost', 8080, '0.0.0.0');//App(hostname, port, 'whoCanConnectIP', '')
+
+//create socket routes
+//route(uri, classInstance, arrOfAllowedOrigins)
+$app->route('/comm', new Comm, $allowed_origins);
+
+//run websocket
+$app->run();
